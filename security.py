06a71024cb7de54b6c4ffb363d8ebd8ec4dc7ea7
Ferenc Bujtas <anothercp3@gmail.com>
2021-12-20 23:46:14 +0100

    hatodik feladat

--- security.py
+++ security.py
@@ -1,14 +1,22 @@
 from werkzeug.security import safe_str_cmp
 from models.user import UserModel
 from models.model_mixin import MixinModel
+import hashlib
 
 
 def authenticate(username, password):
   user = UserModel.find_by_attribute(username=username)
-  if user and safe_str_cmp(user.password, password):
+  if user and safe_str_cmp(user.password, hash_password(password)):
     return user
 
 
 def identity(payload):
   user_id = payload['identity']
   return UserModel.find_by_attribute(id=user_id)
+
+
+def hash_password(password):
+  salt = "23ref9wjdsoi"
+  hashed_password = hashlib.sha512(
+      password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+  return hashed_password
